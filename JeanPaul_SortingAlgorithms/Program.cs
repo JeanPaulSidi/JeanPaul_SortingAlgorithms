88e7aa97d64ca5905806b0a41788f934b204eb6c
JeanPaulSidi <paulsidi@outlook.com>
2024-10-31 17:10:05 -0700

    Add project files.

---
+++ JeanPaul_SortingAlgorithms/Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Diagnostics;
+
+namespace JeanPaul_SortingAlgorithms
+{
+    internal class Program
+    {
+        /* While some sorting algorithms are less performant on large datasets 
+           because they require many comparisons and swaps,
+           others become more efficient thanks to the divide-and-conquer paradigm or the partitioning strategy
+           which decomposes a problem into simpler instances of the same problem and then combines the solutions. */
+
+        static void DisplayRuntime(Stopwatch stopwatch)
+        {
+            TimeSpan ts = stopwatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("Time Taken: " + elapsedTime);
+
+        }
+
+        static void DisplayRuntime(Stopwatch stopwatch, string AlgorithmName)
+        {
+            TimeSpan ts = stopwatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("*************************** RESULTS *****************************");
+            Console.WriteLine();
+            Console.WriteLine(" Algorithm: " + AlgorithmName + "   Time Taken: " + elapsedTime);
+            Console.WriteLine();
+            Console.WriteLine("*****************************************************************");
+        }
+
+        static int[] GenerateRandomArray(int length, int minValue, int maxValue)
+        {
+            Random rand = new Random();
+            int[] array = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = rand.Next(minValue, maxValue);
+            }
+
+            return array;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            string AlgoName;
+            char Choice;
+
+            Console.WriteLine("Generating array...");
+            // usage 100 thousand values
+            stopwatch.Start();
+            int[] largeArr = GenerateRandomArray(100000, 1, 100);
+            stopwatch.Stop();
+            DisplayRuntime(stopwatch);
+
+            Console.WriteLine("Press any key to display the generated array...");
+            Console.ReadKey(true);
+
+            for (int i = 0; i < largeArr.Length; i++)
+            {
+                Console.Write(largeArr[i] + " ");
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("press any key to start sorting...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("  Q. Quick Sort      M. Merge Sort     B. Bubble Sort    I. Insertion Sort");
+            Console.WriteLine();
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine();
+            Console.Write("your choice: ");
+            Choice = char.Parse(Console.ReadLine().ToLower());
+            Console.WriteLine();
+
+
+            switch (Choice)
+            {
+                case 'q':
+                    Console.WriteLine();
+                    AlgoName = "Quick Sort";
+                    Console.WriteLine("starting sorting...");
+                    Console.WriteLine();
+                    stopwatch.Start();
+                    int[] SortedArray = Sort.QuickSortAlgorithm(largeArr, 0, largeArr.Length - 1);
+                    stopwatch.Stop();
+                    DisplayRuntime(stopwatch, AlgoName);
+
+                    Console.WriteLine("Press any key to display the sorted array");
+                    Console.ReadKey(true);
+                    Console.WriteLine();
+
+                    for (int i = 0; i < SortedArray.Length; i++)
+                    {
+                        Console.Write(SortedArray[i] + " ");
+                    }
+                    break;
+
+                case 'm':
+                    Console.WriteLine();
+                    AlgoName = "Merge Sort";
+                    Console.WriteLine("starting sorting...");
+                    Console.WriteLine();
+                    stopwatch.Start();
+                    SortedArray = Sort.MergeSortAlgorithm(largeArr, 0, largeArr.Length - 1);
+                    stopwatch.Stop();
+                    DisplayRuntime(stopwatch, AlgoName);
+
+                    Console.WriteLine("Press any key to display the sorted array");
+                    Console.ReadKey(true);
+                    Console.WriteLine();
+
+                    for (int i = 0; i < SortedArray.Length; i++)
+                    {
+                        Console.Write(SortedArray[i] + " ");
+                    }
+                    break;
+
+                case 'b':
+                    Console.WriteLine();
+                    AlgoName = "Bubble Sort";
+                    Console.WriteLine("starting sorting...");
+                    Console.WriteLine();
+                    stopwatch.Start();
+                    SortedArray = Sort.BubbleSort(largeArr, largeArr.Length);
+                    stopwatch.Stop();
+                    DisplayRuntime(stopwatch, AlgoName);
+
+                    Console.WriteLine("Press any key to display the sorted array");
+                    Console.ReadKey(true);
+                    Console.WriteLine();
+
+                    for (int i = 0; i < SortedArray.Length; i++)
+                    {
+                        Console.Write(SortedArray[i] + " ");
+                    }
+                    break;
+
+                case 'i':
+                    Console.WriteLine();
+                    AlgoName = "Insertion Sort";
+                    Console.WriteLine("starting sorting...");
+                    Console.WriteLine();
+                    stopwatch.Start();
+                    SortedArray = Sort.Insertinsort(largeArr);
+                    stopwatch.Stop();
+                    DisplayRuntime(stopwatch, AlgoName);
+
+                    Console.WriteLine("Press any key to display the sorted array");
+                    Console.ReadKey(true);
+                    Console.WriteLine();
+
+                    for (int i = 0; i < SortedArray.Length; i++)
+                    {
+                        Console.Write(SortedArray[i] + " ");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ JeanPaul_SortingAlgorithms/Sort.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeanPaul_SortingAlgorithms
+{
+    public static class Sort
+    {
+        public static int[] BubbleSort(int[] arr, int n)
+        {
+            int i, j, temp;
+            bool swapped;
+
+            for (i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (swapped == false)
+                {
+                    break;
+                }
+
+            }
+
+            return arr;
+        }
+
+        public static int[] Insertinsort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+
+                arr[j + 1] = key;
+            }
+
+            return arr;
+        }
+
+        //Merge Sort Algorithm
+
+        private static void Merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            for (i = 0; i < n1; ++i)
+            {
+                L[i] = arr[l + i];
+            }
+
+            for (j = 0; j < n2; ++j)
+            {
+                R[j] = arr[m + 1 + j];
+            }
+
+            i = 0;
+            j = 0;
+            int k = l;
+
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+
+                k++;
+            }
+
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        public static int[] MergeSortAlgorithm(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+                MergeSortAlgorithm(arr, l, m);
+                MergeSortAlgorithm(arr, m + 1, r);
+
+                Merge(arr, l, m, r);
+            }
+
+            return arr;
+        }
+
+        //Quick Sort Algorithm
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        private static int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = low - 1;
+
+            for (int j = low; j <= high - 1; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+
+            Swap(arr, i + 1, high);
+            return i + 1;
+        }
+
+        public static int[] QuickSortAlgorithm(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(arr, low, high);
+
+                QuickSortAlgorithm(arr, low, pi - 1);
+                QuickSortAlgorithm(arr, pi + 1, high);
+            }
+
+            return arr;
+        }
+
+
+    }
+}
